454425238e6fa8c113feb07b312599265485106f
amimbs <andymimbs91@gmail.com>
2022-05-31 22:59:35 -0400

    first commit with registration page place holders

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Login() {
+    return (
+        <div className="Login">
+            <h1>Login</h1>
+            <form>
+
+                <input type="text" placeholder="Username"></input>
+
+                <input type="text" placeholder="Password"></input>
+            </form>
+            
+        </div>
+    )
+}
---
+++ src/components/Register.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Register() {
+    return (
+        <div className="registraion">
+            <h1>Register</h1>
+            <form>
+                <input type="text" placeholder="First Name"></input>
+                <input type="text" placeholder="Last Name"></input>
+                <input type="text" placeholder="Username"></input>
+                <input type="text" placeholder="Email"></input>
+                <input type="text" placeholder="Password"></input>
+            </form>
+            
+        </div>
+    )
+}
