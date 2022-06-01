5b91459357a83bbdcd91e1fbb4ff0b7b46a88a4c
amimbs <andymimbs91@gmail.com>
2022-06-01 11:01:09 -0400

    registration is now behaving correctly

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -11,7 +11,8 @@ export default function Register() {
     const [passwordReg, setPasswordReg] = useState('');
 
     const register = () => {
-        axios.post("http://localhost:3001//register", {
+        console.log('i ran')
+        axios.post("http://localhost:3001/register", {
             firstName: firstNameReg,
             lastName: lastNameReg,
             email: emailReg,
@@ -19,6 +20,11 @@ export default function Register() {
             password: passwordReg
         }).then((response) => {
             console.log(response);
+            setFirstNameReg('');
+            setLastNameReg('');
+            setEmailReg('');
+            setUserNameReg('');
+            setPasswordReg('');
         });
     };
 
@@ -27,39 +33,45 @@ export default function Register() {
 
             <h1>Register</h1>
 
-            <form>
+            <div>
                 <input
                     type="text"
                     placeholder="First Name"
+                    value={firstNameReg}
                     onChange={(e) => { setFirstNameReg(e.target.value) }}>
                 </input>
 
                 <input
                     type="text"
                     placeholder="Last Name"
+                    value={lastNameReg}
                     onChange={(e) => { setLastNameReg(e.target.value) }}>
                 </input>
 
                 <input
                     type="text"
                     placeholder="Username"
+                    value={userNameReg}
                     onChange={(e) => { setUserNameReg(e.target.value) }}>
                 </input>
 
                 <input
                     type="text"
                     placeholder="Email"
+                    value={emailReg}
                     onChange={(e) => { setEmailReg(e.target.value) }}>
                 </input>
 
                 <input
                     type="text"
                     placeholder="Password"
+                    value={passwordReg}
                     onChange={(e) => { setPasswordReg(e.target.value) }}>
                 </input>
 
-                <button onSubmit={register}> Register </button>
-            </form>
+                {/* this prevents the onlick from running immediately on page load */}
+                <button type="submit" onClick={register}> Register </button>
+            </div>
 
         </div>
     )
